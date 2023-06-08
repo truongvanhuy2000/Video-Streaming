c7691f4839d46e078715e57afec463b27928b10b
huy <truongvanhuy2000@gmail.com>
2023-06-08 11:40:05 +0700

    add name to nfsvolume

--- loadBalancerApp/loadBalancer/dockerClient/dockerClient.py
+++ loadBalancerApp/loadBalancer/dockerClient/dockerClient.py
@@ -1,51 +1,60 @@
 import docker
 import yaml
 from yaml.loader import SafeLoader
+import logging
+
 # Data stucture that represent video server configuration
 class videoServer:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, config, client):
+        self.config = config
+        self.client = client
+
+    def __readConfig(self):
+        videoServerConfig = self.config.get('videoserver')
+        
+        self.image = videoServerConfig.get('image')
+        self.networks = videoServerConfig.get('networks')
+        self.env = videoServerConfig.get('environment')
+
+        volumes = videoServerConfig.get('volumes')[0]
+        self.mounts = ["{0}:{1}:ro".format(volumes.get('source'), volumes.get('target'))]
+
+    def replicate(self, node: str):
+        self.__readConfig()
+
+        nodePlacement = ["node.id == {}".format(node)]
+        service_spec = {
+            'image' : self.image, 
+            'networks' : self.networks, 
+            'env' : self.env, 
+            'mounts' : self.mounts,
+            'constraints' : nodePlacement
+        }
+        print(service_spec)
+        try:
+            service = self.client.services.create(**service_spec)
+            serviceAttr = self.client.services.get(service.id)
+        except Exception:
+            logging.error("Can't perform docker service operation")
+            exit()
+        return serviceAttr.name
 
 class dockerClient:
     #serverAddr='host.docker.internal:2375'
     #configDir='loadBalancer/dockerClient/config/videoServer.yaml'
     def __init__(self, serverAddr, configDir) -> None:
         self.client = docker.DockerClient(base_url=serverAddr)
-        with open(configDir) as f:
-            self.config = yaml.load(f, Loader=SafeLoader)
+        self.configDir = configDir
  
-    def createVideoServer(self, node: str):
-        video_server = self.readVideoServerConfig()
-        nodePlacement = ["node.id == {}".format(node)]
-        try:
-            service = self.client.services.create(image=video_server.image, 
-                                              networks=video_server.networks, 
-                                              env=video_server.env, 
-                                              mounts=video_server.mounts,
-                                              constraints=nodePlacement)
-        except Exception:
-            print("Cant create service")
-            exit()
-        service = self.client.services.get(service.id)
-        # Remember to implement code to get service ip address
+    def createService(self, node:str, service:str):
+        with open(self.configDir + service + '.yaml') as f:
+            if f is None:
+                logging.error("Can't open file")
+                return None
+            config = yaml.load(f, Loader=SafeLoader)
+        match service:
+            case 'videoServer':
+                video_service = videoServer(config, self.client)
+                return video_service.replicate(node=node)
 
-        # Remember to implement code to get service ip address
-        return service.attrs
 
-    def readVideoServerConfig(self):
-        videoServerConfig = self.config.get('videoserver')
-        
-        image = videoServerConfig.get('image')
-        networks = [].append(videoServerConfig.get('networks'))
-        env = [].append(videoServerConfig.get('environment'))
-        volumes = [].append(videoServerConfig.get('volumes'))
-        mounts = [].append("{0}:{1}:ro".format(volumes.get('source'), volumes.get('target')))
-
-        return videoServer(image=image, 
-                           networks=networks, 
-                           env=env, 
-                           volumes=volumes, 
-                           mounts=mounts)
-
-        
--- loadBalancerApp/test/unit/dockerClient/testDockerClient.py
+++ loadBalancerApp/test/unit/dockerClient/testDockerClient.py
@@ -3,11 +3,10 @@ from loadBalancer.dockerClient.dockerClient import dockerClient
 
 class dockerClientTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.dockerClient = dockerClient('192.168.56.20:2375', 'test/unit/dockerClient/config/videoServer.yaml')
+        self.dockerClient = dockerClient('192.168.56.20:2375', 'loadBalancer/dockerClient/config/')
 
     def testCreateVideoServer(self):
-        with self.assertRaises(Exception):
-            output = self.dockerClient.createVideoServer('helusms5yiiqyk4elmni8ox4a')
-
+        output = self.dockerClient.createService(node='3sy2t6skhgpcle5c94sou3tj0', service='videoServer')
+        self.assertIsNotNone(output)
 if __name__ == '__main__':
     unittest.main()
