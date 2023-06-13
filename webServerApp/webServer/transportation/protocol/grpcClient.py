0c769551f0b5baab9846236a44bb6b37a52356a0
huy <truongvanhuy2000@gmail.com>
2023-06-13 15:27:51 +0700

    test quart implementation

--- webServerApp/webServer/transportation/protocol/grpcClient.py
+++ webServerApp/webServer/transportation/protocol/grpcClient.py
@@ -16,6 +16,8 @@ class grpcClient(clientProtocol):
 
         self.channel = grpc.insecure_channel(addr, options=channel_opt)
         self.stub = image_pb2_grpc.image_tranferStub(channel=self.channel)
+        asyncio.run(self.waitForServer())
+
 
     async def waitForServer(self):
         while True:
